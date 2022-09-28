dd21efdfcd55b0b6f5701a0f061b1cabd3afd484
keeta <mario3.barcena@gmail.com>
2022-09-27 22:12:15 -0500

    Previo a la entrega de tarea M7
    Se hizo intento de realizar tarea de M7 pero se mandaron dudas

--- ParalatareaEBAC/Assets/Modulo6/Scrips/ColorCapsula.cs
+++ ParalatareaEBAC/Assets/Modulo6/Scrips/ColorCapsula.cs
@@ -4,11 +4,24 @@ using UnityEngine;
 
 public class ColorCapsula : MonoBehaviour
 {
+    bool variable1;
+    
+
+    // A B C   OR  AND  ((A or B) and c))  ((A or B) or c))
+    // 1 1 1    1   1            1                  1
+    // 1 1 0    1   0            0                  1
+    // 1 0 1    1   0            1                  1
+    // 1 0 0    1   0            0                  1
+    // 0 1 1    1   0            1                  1
+    // 0 1 0    1   0            0                  1
+    // 0 0 1    1   0            0                  1
+    // 0 0 0    0   0            0                  0
+
     public GameObject Capsulaprueba;
     // Start is called before the first frame update
     void Start()
     {
-        
+        variable1 = false;
     }
 
     // Update is called once per frame
@@ -19,8 +32,25 @@ public class ColorCapsula : MonoBehaviour
 
     private void FixedUpdate()
     {
-        Color capsula = new Color(Random.value, Random.value, Random.value);
-        Capsulaprueba.GetComponent<MeshRenderer>().material.color = capsula;
+        //Color capsula = new Color(Random.value, Random.value, Random.value);
+        //Capsulaprueba.GetComponent<MeshRenderer>().material.color = capsula;
         
+        if (variable1 == true)
+        {
+            Color capsula = new Color(0, 0, 0);
+            Capsulaprueba.GetComponent<MeshRenderer>().material.color = capsula;
+            Debug.Log("Hola");
+            variable1 = false;
+        }
+        if (variable1 == false)
+        {
+            Color capsula = new Color(1, 1, 1);
+            Capsulaprueba.GetComponent<MeshRenderer>().material.color = capsula;
+            Debug.Log("se esta cumpliendo la condición");
+            variable1 = true;
+        }
+
+
+
     }
 }
