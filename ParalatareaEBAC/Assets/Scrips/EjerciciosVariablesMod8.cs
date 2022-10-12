e2548b085723def36cafcbd1f2f8fe97ec87db22
keeta <mario3.barcena@gmail.com>
2022-10-11 19:31:23 -0500

    Avances en tarea de Modulo 8
    Se realizaron pocos cambios para la tarea de m8

--- ParalatareaEBAC/Assets/Scrips/EjerciciosVariablesMod8.cs
+++ ParalatareaEBAC/Assets/Scrips/EjerciciosVariablesMod8.cs
@@ -12,13 +12,51 @@ public class EjerciciosVariablesMod8 : MonoBehaviour
     int cambio = 0;
     int valor5 = 50;
     int valor6 = 15;
+    int liminf = 0;
+    int limisup = 2;
+    int valorswitch = 2;
+    string unstring = "Blanco";
+    string dostring = "Negro";
+    float numero = 3.14159f;
+    string valor7;  
+    enum palabra 
+    {
+        unstring,
+        dostring
+    }
     public GameObject colorcapsula;
     // Start is called before the first frame update
     void Start()
     {
         resultado = valor3 - valor4;
-        Debug.Log("El resultado es"+ resultado);
+        Debug.Log("El resultado es " + resultado);
+        valorswitch = Random.Range(liminf, limisup);
+        switch (valorswitch)
+        {
+            case (int)palabra.unstring:
+                colorcapsula.GetComponent<MeshRenderer>().material.color = Color.white;
+                Debug.Log("El color seleccionado es " + unstring);
+                break;
+            case (int)palabra.dostring:
+                colorcapsula.GetComponent<MeshRenderer>().material.color = Color.black;
+                Debug.Log("El color seleccionado es " + dostring);
+                break;
+        }
+        string exflt = numero.ToString();
+        Debug.Log("Este es tu numero " + (string.Format(exflt, "{0:0.0000}", numero)));
 
+        string minombrecom = "Mario Barcena Calderon";
+        string minombre = minombrecom.Substring(0, 5);
+        string miapellidopat = minombrecom.Substring(6, 8);
+        string miapellidomat = minombrecom.Substring(10, 12);
+        Debug.Log(minombrecom);
+        Debug.Log(minombre);
+        Debug.Log(miapellidopat);
+        Debug.Log(miapellidomat);
+
+        char misletras;
+        string frase = "Hola Mundo";
+        
     }
 
     // Update is called once per frame
@@ -32,15 +70,16 @@ public class EjerciciosVariablesMod8 : MonoBehaviour
     {
         valor2 *= 2;
         Debug.Log(valor2);
+
         cambio = Random.Range(valor5 , valor6);
         if (cambio %2 == 0)
         {
-            Debug.Log("El numero " +cambio+ "es par");
+            Debug.Log("El numero " +cambio+ " es par");
             colorcapsula.GetComponent<MeshRenderer>().material.color = Color.red;
         }
         else
         {
-            Debug.Log("El numero " + cambio + "es non");
+            Debug.Log("El numero " + cambio + " es non");
             colorcapsula.GetComponent<MeshRenderer>().material.color = Color.blue;
         }
 
