a054c8c3c8eb2fc556242163cc84703a6ef4cd5a
keeta <mario3.barcena@gmail.com>
2022-10-20 22:42:35 -0500

    Avances en tarea M9
    Se entregan avances en la tarea del M9

--- ParalatareaEBAC/Assets/Scrips/EjerciciosCiclosyArreglos.cs
+++ ParalatareaEBAC/Assets/Scrips/EjerciciosCiclosyArreglos.cs
@@ -13,33 +13,63 @@ public class EjerciciosCiclosyArreglos : MonoBehaviour
     int limite2 = 10;
     int x;
     int y;
+    int contador4 = 3;
+    int limite3 = 0;
+    int limite4 = 0;
+    int contador5 = 0;
     // Start is called before the first frame update
     void Start()
     {
-        int[] arreglo1 = new int[contador];
-        for (int x = 0; x < arreglo1.Length; x++) 
-        {
-           arreglo1[x] = contador = Random.Range(limitinf, limitsup);
-            Debug.Log("Numeros del arreglo 1 " + arreglo1[x]);
-            contador--;
-        }
+        //int[] arreglo1 = new int[contador];
+        //for (int x = 0; x < arreglo1.Length; x++) 
+        //{
+        //   arreglo1[x] = contador = Random.Range(limitinf, limitsup);
+        //    Debug.Log("Numeros del arreglo 1 " + arreglo1[x]);
+        //    contador--;
+        //}
 
-        int[] arreglo2 = new int[contador2];
-        for (int y = 0; y < arreglo2.Length; y++)
-        {
-            arreglo2[y] = contador = Random.Range(limite1, limite2);
-            Debug.Log("Numeros del arreglo 2 " + arreglo2[y]);
-            contador2--;
-        }
+        //int[] arreglo2 = new int[contador2];
+        //for (int y = 0; y < arreglo2.Length; y++)
+        //{
+        //    arreglo2[y] = contador = Random.Range(limite1, limite2);
+        //    Debug.Log("Numeros del arreglo 2 " + arreglo2[y]);
+        //    contador2--;
+        //}
 
-        int[] arreglo3 = new int[contador3];
-        for (int z = 0; z < arreglo3.Length; z++)
+        //int[] arreglo3 = new int[contador3];
+        //for (int z = 0; z < arreglo3.Length; z++)
+        //{
+        //    arreglo3[z] = arreglo1[x] + arreglo2[y];
+        //    Debug.Log("Numeros de arreglo 3 " + arreglo3[z]);
+        //    x++;
+        //    y++;
+        //    contador3--;
+        //}
+        //string[] c = new string[3] { "Hola", "A", "Todos" };
+        //int[] arreglo4 = new int[contador4];
+        //for (int a = 0; a < c.Length; a++) 
+        //{
+        //   arreglo4[a] = a + 1;
+        //    Debug.Log(c[a]);
+        //}
+        //foreach (string j in c)
+        //{
+        //    Debug.Log(j);
+        //}
+        int[,] matriz = new int[2, 3] { { 1, 2, 3 }, { 4, 5, 6 } };
+        for (int k = 0; k < matriz.GetLength(0); k++)
         {
-            arreglo3[z] = arreglo1[x] + arreglo2[y];
-            Debug.Log("Numeros de arreglo 3 " + arreglo3[z]);
-            x++;
-            y++;
-            contador3--;
+            for (int l = 0; l < matriz.GetLength(1); l++)
+            {
+                Debug.Log("Numeros de matriz 1 " + matriz[k, l]);
+            }
+        }
+        int[] arreglo5 = new int[contador5];
+        for (int b = 0; b < arreglo5.Length; b++)
+        {
+            arreglo5[b] = contador5 = Random.Range(limite3, limite4);
+            Debug.Log("Numeros para multiñlicación " + arreglo5[b]);
+            contador5--;
         }
 
     }
